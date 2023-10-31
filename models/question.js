7c43bd8294e1a563cc52f643162ef762eac44919
Greg Mattia <gjmattia@gmail.com>
2023-10-30 20:32:05 -0400

    edit question schema to allow more characters

--- models/question.js
+++ models/question.js
@@ -5,8 +5,8 @@ const Schema = mongoose.Schema
 const questionSchema = new Schema({
     user: { type: Schema.Types.ObjectId, ref: 'user' },
     category: { type: String, required: true },
-    question: { type: String, required: true, minLength: 1, maxLength: 50 },
-    correct_answer: { type: String, required: true, minLength: 1, maxLength: 50 },
+    question: { type: String, required: true, minLength: 1, maxLength: 80 },
+    correct_answer: { type: String, required: true, minLength: 1, maxLength: 80 },
     incorrect_answers: { type: [String] },
     correct: { type: Number, default: 0 },
     incorrect: { type: Number, default: 0 }
